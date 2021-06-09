391db31d1af894308c308d208589c93c5b124cfd
Syamil28 <62102884+Syamil28@users.noreply.github.com>
2021-06-09 20:37:00 +0900

    Create Main.java

---
+++ src/pacman/Main.java
@@ -0,0 +1,51 @@
+package pacman;
+
+import java.awt.CardLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * Project Kuis 2 PAA E
+ * Main class untuk project ini.
+ * 
+ * @author Daanii Kusnanta
+ * @author M. Nur Abdurrauf
+ * @author Syamil Dhifaul Haq Syukur
+ */
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+
+	public static final int WIDTH = Level.TILESIZE*(2 + Level.TILES_X);
+    public static final int HEIGHT = Level.TILESIZE*(5 + Level.TILES_Y);
+    public static CardLayout cardLayout;
+    public static JPanel mainPanel;
+
+    public Main() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        mainPanel.add(new Level(), "level");
+        
+        add(mainPanel);
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Pacman");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            Main ex = new Main();
+            ex.setVisible(true);
+        });
+    }
+}
